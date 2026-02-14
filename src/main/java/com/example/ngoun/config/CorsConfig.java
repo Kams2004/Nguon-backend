03bc7c777ad8880596f7014e735bc1db5b12d92c
kams2004 <kamsuperold2004@gmail.com>
2026-02-14 11:12:48 +0100

    updates

--- src/main/java/com/example/ngoun/config/CorsConfig.java
+++ src/main/java/com/example/ngoun/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:8080",
                 "http://10.237.18.152:8080",
-                "http://www.nguonevents.com/"
+                "http://www.nguonevents.com/",
                 "http://167.86.120.214"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
